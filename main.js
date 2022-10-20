ce585e3081738d016bf9fd910c747693e6349a87
Kateryna Povaliaieva <katerina.poval@gmail.com>
2022-10-20 20:39:45 +0300

    8

---
+++ main.js
@@ -0,0 +1,18 @@
+$(function () {
+
+    $('.rev_slider').slick({
+        arrows: false,
+        dots: true,
+        centerMode: true,
+        slidesToShow: 3,
+        autoplay: true,
+        speed: 1000,
+        // focusOnChange: true,
+        infinite: false,
+
+
+
+    });
+
+
+})
