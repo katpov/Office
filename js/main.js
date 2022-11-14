3c9dae25f684d212dffe0398b73f1c5a13c42cca
Kateryna Povaliaieva <katerina.poval@gmail.com>
2022-11-14 11:15:01 +0200

    last

--- js/main.js
+++ js/main.js
@@ -1,19 +1,50 @@
 $(function () {
 
+    $('.cards_slider').slick({
+        arrows: false,
+        slidesToShow: 3,
+        // focusOnChange: true,
+        infinite: false,
+        variableWidth: true,
+        responsive: [{
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    infinite: true
+                }
+            }]
+    });
+})
+
+$(function () {
+
     $('.rev_slider').slick({
         arrows: false,
         dots: true,
         centerMode: true,
         centerPadding: '0px',
         slidesToShow: 3,
+        slidesToScroll: 3,
         autoplay: true,
         speed: 1000,
         // focusOnChange: true,
-        infinite: false,
+        infinite: true,
         variableWidth: true,
+        responsive: [{
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
     });
 })
 
 $('.rev_slider').on('afterChange', function(){
 
 } )
+
+
+
